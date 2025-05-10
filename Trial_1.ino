7a80a136e5e7758dd03daab8e948f695a41e0248
NeroXD369 <150569553+NeroXD369@users.noreply.github.com>
2025-05-10 22:39:48 +0800

    Trial_1.ino

--- Trial_1.ino
+++ Trial_1.ino
@@ -8,22 +8,9 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial); // Wait for Serial Monitor to open
-
-  SPI.begin();            // Init SPI bus
-  rfid.PCD_Init();        // Init MFRC522
-  Serial.println("RFID reader initialized.");
-
-  // Check firmware version to confirm module is connected
-  byte version = rfid.PCD_ReadRegister(rfid.VersionReg);
-  Serial.print("Firmware Version: ");
-  Serial.println(version, HEX);
-
-  if (version == 0x00 || version == 0xFF) {
-    Serial.println("ERROR: RFID reader not connected or not functioning.");
-  } else {
-    Serial.println("Scan an RFID tag...");
-  }
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.println("Scan an RFID tag...");
 }
 
 void loop() {
@@ -32,7 +19,7 @@ void loop() {
     return;
   }
 
-  // Print UID of the tag
+  // Print UID
   Serial.print("UID tag: ");
   for (byte i = 0; i < rfid.uid.size; i++) {
     Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -40,8 +27,7 @@ void loop() {
   }
   Serial.println();
 
-  // Halt PICC communication and stop encryption
+  // Halt PICC
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
 }
-
