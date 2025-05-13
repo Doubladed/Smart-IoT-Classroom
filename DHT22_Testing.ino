1dc9f32da3dd9ae0931babeab0c1c11439118dcf
NeroXD369 <150569553+NeroXD369@users.noreply.github.com>
2025-05-13 18:52:44 +0800

    Create DHT22_Testing.ino
    Will add the fan code

---
+++ DHT22_Testing.ino
@@ -0,0 +1,30 @@
+#include "DHT.h"
+
+#define DHTPIN 3        // Digital pin connected to the DHT22
+#define DHTTYPE DHT22    // DHT 22 (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000); // Delay between readings
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" °C");
+}
