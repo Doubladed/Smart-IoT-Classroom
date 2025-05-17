dd3f5f28cfbf370a20192f186604eeffff09d883
NeroXD369 <150569553+NeroXD369@users.noreply.github.com>
2025-05-17 18:48:21 +0800

    Update RFID_Testing.ino

--- RFID_Testing.ino
+++ RFID_Testing.ino
@@ -3,10 +3,11 @@
 
 #define SS_PIN 10
 #define RST_PIN 9
+#define LED_PIN 8  // LED to indicate at least one person inside
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-
+// Structure to hold UID + user data and state
 struct User {
   byte uid[4];
   const char* name;
@@ -14,7 +15,7 @@ struct User {
   bool isInside; // false = outside, true = inside
 };
 
-
+// Predefined UID database
 User users[] = {
   {{0x93, 0xEA, 0x90, 0x28}, "Sakkaf", "1201102450", false},
   {{0xC3, 0x0A, 0xD4, 0x2C}, "Ghaleb", "1201102570", false}
@@ -24,24 +25,35 @@ const int userCount = sizeof(users) / sizeof(users[0]);
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial); 
 
   SPI.begin();
   rfid.PCD_Init();
-  delay(4); 
-  rfid.PCD_SetAntennaGain(rfid.RxGain_max); //based on the github this makes it more accurate and based on my testting it semi functions 
+  delay(4); // Some boards need time after init
+
+  // Set antenna gain to maximum
+  rfid.PCD_SetAntennaGain(rfid.RxGain_max);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // LED off initially
 
   Serial.println("RFID reader initialized.");
-  Serial.println("Scan an RFID tag...");
+ byte version = rfid.PCD_ReadRegister(rfid.VersionReg);
+  Serial.print("Firmware Version: ");
+  Serial.println(version, HEX);
+
+  if (version == 0x00 || version == 0xFF) {
+    Serial.println("ERROR: RFID reader not connected or not functioning.");
+  } else {
+    Serial.println("Scan an RFID tag...");
+  }
 }
 
 void loop() {
-
   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
     return;
   }
 
-
+  // Show the UID
   Serial.print("UID tag: ");
   for (byte i = 0; i < rfid.uid.size; i++) {
     Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -49,8 +61,8 @@ void loop() {
   }
   Serial.println();
 
-  
   bool found = false;
+
   for (int i = 0; i < userCount; i++) {
     bool match = true;
     for (byte j = 0; j < 4; j++) {
@@ -83,7 +95,17 @@ void loop() {
     Serial.println("Unknown tag. Access denied.");
   }
 
-  
+  // Update LED state
+  bool anyoneInside = false;
+  for (int i = 0; i < userCount; i++) {
+    if (users[i].isInside) {
+      anyoneInside = true;
+      break;
+    }
+  }
+  digitalWrite(LED_PIN, anyoneInside ? HIGH : LOW);
+
+  // Halt card communication
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
 }
