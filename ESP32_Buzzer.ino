8c6aa76382735150f4fb0924c944eab535b42e6e
Mohammed Waleed Khan <149855667+waleedk7@users.noreply.github.com>
2025-06-02 17:43:54 +0800

    Add files via upload

---
+++ ESP32_Buzzer.ino
@@ -0,0 +1,16 @@
+#define buzzer 32
+
+void setup(){
+   
+  pinMode(buzzer, OUTPUT); 
+
+}
+
+void   loop(){
+ 
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);         // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);         // ...for 1sec
+  
+}
